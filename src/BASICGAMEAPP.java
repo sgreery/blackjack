feab21aa61920b8cac24b0a62044a1303538763f
sgreery <samuel_greer27@milton.edu>
2025-05-23 08:42:53 -0400

    a

--- src/BASICGAMEAPP.java
+++ src/BASICGAMEAPP.java
@@ -10,17 +10,24 @@ public class BASICGAMEAPP {
     public BASICGAMEAPP(){
         deck = new Card[13];
         System.out.println("Welcome to Sammy's casino");
+        int counter = 0;
         for(int all = 0; all < 4; all++){
             for(int x=0; x<deck.length; x++){
                 Card c = new Card(x+1, all);
 
-                deck[x] = c;
-                c.print();
+                deck[counter] = c;
+//                c.print();
+                deck[counter].print();
 
             }
         }
+        System.out.println("*");
+        printDeck();
 
 
+
+    }
+    public void printDeck(){
 
     }
 }
--- src/Card.java
+++ src/Card.java
@@ -9,12 +9,69 @@ public class Card {
         suit = "Spades";
         isEleven = true;
         value = 11;
+        if(psuit==0){
+            suit = "diamonds";
+        }
+        if(psuit==1){
+            suit = "spades";
+        }
         if(psuit==2){
             suit = "hearts";
+        }
+        if(psuit==3){
+            suit = "clubs";
         }
         if(pnumber == 13){
             number = "King";
             value = 10;
+        }
+        if(pnumber == 12){
+            number = "Queen";
+            value = 10;
+        }
+        if(pnumber == 11){
+            number = "Jack";
+            value = 10;
+        }
+        if(pnumber == 10){
+            number = "Ten";
+            value = 10;
+        }
+        if(pnumber == 9){
+            number = "Nine";
+            value = 9;
+        }
+        if(pnumber == 8){
+            number = "Eight";
+            value = 8;
+        }
+        if(pnumber == 7){
+            number = "Seven";
+            value = 7;
+        }
+        if(pnumber == 6){
+            number = "Six";
+            value = 6;
+        }
+        if(pnumber == 5){
+            number = "Five";
+            value = 5;
+        }
+        if(pnumber == 4){
+            number = "Four";
+            value = 4;
+        }
+        if(pnumber == 3){
+            number = "Three";
+            value = 3;
+        }
+        if(pnumber == 2){
+            number = "Two";
+            value = 2;
+        }
+        if(pnumber == 1){
+            number = "Ace";
+            value = 11;
         }
     }
     public void print(){
